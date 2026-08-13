d92b3d57b21aaf3c7f484bad43ee75cf8af0ca87
ken303867-png <ken303867@gmail.com>
2026-08-13 21:07:26 +0900

    Refresh cloze backup cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'swallow-cloze-614-new-';
-const CACHE = CACHE_PREFIX + 'v8-transfer';
+const CACHE = CACHE_PREFIX + 'v9-backup';
 const ASSETS = ['./', './index.html', './manifest.json', './results-patch.js?v=1', './data-transfer.js'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
