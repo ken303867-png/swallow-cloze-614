00d55c1448cb08a92d1ccb6087362c3e863b9627
ken303867-png <ken303867@gmail.com>
2026-08-19 23:12:03 +0900

    Cache B2 cloze design QA patch

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_PREFIX = 'swallow-cloze-614-new-';
-const CACHE = CACHE_PREFIX + 'v18-compact1390-qaSAB1';
+const CACHE = CACHE_PREFIX + 'v19-compact1390-qaSAB2';
 const ASSETS = [
   './','./index.html','./manifest.json','./styles.css',
   './data-sections-01-05.js','./data-sections-06-09.js','./data-sections-10-13.js','./data-sections-14-17.js',
-  './data.js','./qa-data-patch.js','./qa-b-context-patch.js','./core.js','./qa-progress-migration.js','./data-transfer.js','./results.js','./memory.js'
+  './data.js','./qa-data-patch.js','./qa-b-context-patch.js','./qa-b-design-patch.js','./core.js','./qa-progress-migration.js','./data-transfer.js','./results.js','./memory.js'
 ];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
